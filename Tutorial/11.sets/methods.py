c5d7d0a2317a7dcf1c74be012c94b36482dfcc82
shakil-babu <personal.shakil.babu@gmail.com>
2022-01-25 11:11:53 +0600

    sets-methods

---
+++ Tutorial/11.sets/methods.py
@@ -0,0 +1,35 @@
+# issubset method
+# x = {'a', 'b','c'}
+# y = {'f','e','a','k','b','p','c'}
+
+# z = x.issubset(y)
+# print(z) # output: True
+
+
+# issuperset method
+# x = {1, 2, 3, 223, 4}
+# y = {1, 2}
+
+# z = x.issuperset(y)
+# print(z)  # output: True
+
+
+# symmetric_difference method
+# x = {"a", 'b', 'c', 'd'}
+# y = {'a', 'b', 'd'}
+
+# z = x.symmetric_difference(y)
+# print(z)
+# output : {"c"}
+
+# x = {"a", 'b', 'c', 'd'}
+# y = {'a', 'b', 'd'}
+# x.symmetric_difference_update(y)
+# print(x)
+
+
+# union method
+# x = {"a", "b", "c"}
+# y = {"d", "e", "f"}
+# z = y.union(x)
+# print(z)
