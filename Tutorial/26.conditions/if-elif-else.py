5f89805bae62a50dff3414a5256f2afa973cdb49
shakil-babu <personal.shakil.babu@gmail.com>
2022-01-25 10:41:54 +0600

    conditions

---
+++ Tutorial/26.conditions/if-elif-else.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first : "))
+b = int(input("Enter second : "))
+c = int(input("Enter third : "))
+
+if a > b and a > c :
+    print(a)
+elif b > a and b > c :
+    print(b)
+else :
+    print(c)
