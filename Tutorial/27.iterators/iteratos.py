ab65fc5bc70f3cf4c22dd69d821ba3aea0e96a5b
shakil-babu <personal.shakil.babu@gmail.com>
2022-01-25 11:30:28 +0600

    iterators

---
+++ Tutorial/27.iterators/iteratos.py
@@ -0,0 +1,18 @@
+tup = ('apple', 'banana', 'orange')
+ite = iter(tup)
+
+print(next(ite))
+print(next(ite))
+print(next(ite))
+
+
+# in string
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
---
+++ Tutorial/27.iterators/looping_through_an_iterator.py
@@ -0,0 +1,10 @@
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+    print(x)
+
+
+mystr = "banana"
+
+for x in mystr:
+    print(x)
