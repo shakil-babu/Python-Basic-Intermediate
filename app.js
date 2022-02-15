0f6d5a759b22b166f44b6991b402a9b6a56a64eb
shakil-babu <personal.shakil.babu@gmail.com>
2022-02-15 11:09:29 +0600

    linked list

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-var transpose = function (matrix) {
-  let ans = [];
-  for (let i = 0; i < matrix[0].length; i++) {
-    let level = [];
-    for (let j = 0; j < matrix.length; j++) {
-      level.push(matrix[j][i]);
-    }
-
-    ans.push(level);
+var hasAlternatingBits = function (n) {
+  let bin = n.toString(2);
+  if (bin.includes("00") || bin.includes("11")) {
+    return false;
   }
-
-  return ans;
+  return true;
 };
 
-let ans = transpose([
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-]);
+let ans = hasAlternatingBits(6);
 console.log(ans);
